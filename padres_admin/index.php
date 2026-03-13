7b7024a5e41a99f39e98b6ac6bee264109f03885
JoseFabianmunozcano <j.muxozcano.g656@edu.utc.mx>
2026-03-12 23:06:53 -0600

    generic

--- padres_admin/index.php
+++ padres_admin/index.php
@@ -94,5 +94,8 @@ function confirmarEliminacion(id) {
 
 <?php 
 $conexion->close();
-include '../includes/footer.php'; 
 ?>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
